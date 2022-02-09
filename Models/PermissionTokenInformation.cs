d8e94ca75d80d4d848c04fd08ff5fa914d585d27
Alastair <alastair.w.little@gmail.com>
2022-02-09 22:39:20 +0000

    Remove un-necessary file.

--- Models/PermissionTokenInformation.cs
+++
@@ -1,8 +0,0 @@
-namespace PermissionsMiddleware.Models;
-
-    public class PermissionTokenInformation
-    {
-        public Guid UserId { get; set; }
-        public string Area { get; set; } = null!;
-        public string Action { get; set; } = null!;
-    }
